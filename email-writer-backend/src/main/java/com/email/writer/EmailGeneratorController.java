88342d29ea681436c7b27f057e45dfbc9d1d3214
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2025-03-01 14:09:27 +0530

    formatted

--- email-writer-backend/src/main/java/com/email/writer/EmailGeneratorController.java
+++ email-writer-backend/src/main/java/com/email/writer/EmailGeneratorController.java
@@ -1,7 +1,6 @@
 package com.email.writer;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- email-writer-backend/src/main/java/com/email/writer/EmailGeneratorService.java
+++ email-writer-backend/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -2,7 +2,6 @@ package com.email.writer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
